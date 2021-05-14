d138458b7b9c07feb1e30316b6480bf65932f05b
Roque Ayala Angel David <ingRoque98@gmail.com>
2021-05-14 15:24:09 -0500

    Generando componentes con schematics material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialRoutingModule } from './material-routing.module';
-import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -19,7 +24,23 @@ import {MatCardModule} from '@angular/material/card';
     MatIconModule,
     MatBadgeModule,
     MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatListModule
   ],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule,MatCardModule],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatListModule
+  ],
 })
 export class MaterialModule {}
